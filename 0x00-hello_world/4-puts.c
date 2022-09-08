25745c13bf17cedabc3bbaa7044ee11034ec0a8b
Devkamara <chegerah@gmail.com>
2022-09-08 15:32:41 -0700

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Description: main - Prints out a string
+ * Return: 0 if true
+ */
+int main(void)
+{
+char Mystr[] = "\"Programming is like building a multilingual puzzle";
+puts(Mystr);
+return (0);
+}
