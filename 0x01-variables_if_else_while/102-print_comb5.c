9fc6f9ceb0f6f17450523ac978437d1ec15f4b86
Devkamara <chegerah@gmail.com>
2022-09-09 20:57:15 -0700

    additional tasks

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,48 +1,37 @@
 #include <stdio.h>
+#include<string.h>
+#define N 10
 /**
  *main - prints possible of two two digits
  *
  *Return: 0 if true
  */
-int main(void)
-{
-int c;
-int d;
-int e;
-int f = 0;
 
-while (f < 10)
+void print(int *num, int n)
 {
-e = 0;
-while (e < 10)
+    int i;
+    for ( i = 0 ; i < n ; i++)
+        printf("%d ", num[i]);
+    printf("\n");
+}
+int main()
 {
-d = 0;
-while (d < 10)
-{
-c = 0;
-while (c < 10)
-{
-if (!(f == c && e == d))
-{
-putchar('0' + f);
-putchar('0' + e);
-putchar(' ');
-putchar('0' + d);
-putchar('0' + c);
-if (!(f + e == 18 && c + d == 17 && d == 9))
-{
-putchar(',');
-putchar(' ');
-}
-}
-c++;
-}
-d++;
-}
-e++;
-}
-f++;
-}
-putchar('\n');
-return (0);
+    int num[N];
+    int *ptr;
+    int temp;
+    int i, n, j;
+    printf("\nHow many number you want to enter: ");
+        scanf("%d", &n);
+    printf("\nEnter a list of numbers to see all combinations:\n");
+    for (i = 0 ; i < n; i++)
+        scanf("%d", &num[i]);
+    for (j = 1; j <= n; j++) {
+        for (i = 0; i < n-1; i++) {
+            temp = num[i];
+            num[i] = num[i+1];
+            num[i+1] = temp;
+            print(num, n);
+	}
+    }
+    return 0;
 }
