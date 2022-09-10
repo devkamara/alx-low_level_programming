1ec8396c65c4457739998a761d7d75686051c367
Devkamara <chegerah@gmail.com>
2022-09-09 20:23:30 -0700

    I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints alphabet in lowercase
+ * Return: 0 if true (success)
+ */
+
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if ch== 'e' || ch == 'q')
+{
+continue;
+}
+putchar(ch);
+}
+putchar ('\n');
+return (0);
+
+}
