3dee4b2036e46b73c43161d467e24f9e9c2057a5
Devkamara <chegerah@gmail.com>
2022-09-20 10:45:00 -0700

    98 Battery St

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ *  *reset_to_98 - takes a pointer to an int as para & updates the value to 98
+ *   *@n: pointer to be checked
+ *    */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
