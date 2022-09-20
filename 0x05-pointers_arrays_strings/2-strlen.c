f189afe10b04905cbc001a07d3e978075c0b4dd5
Devkamara <chegerah@gmail.com>
2022-09-20 10:59:25 -0700

    puts and print rev

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- *_strlen - returns the lengh of a string
- *@s: string to be checked
- *Return: length of the string
- */
+*_strlen - returns the lengh of a string
+*@s: string to be checked
+*Return: length of the string
+*/
 int _strlen(char *s)
 {
 	int length = 0;
+
 	while (s[length])
 		length++;
 	return (length);
