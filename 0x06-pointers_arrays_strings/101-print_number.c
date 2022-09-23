c1a578a1ed15d06fb0e6575ef3962cc3ddecf97f
Devkamara <chegerah@gmail.com>
2022-09-22 19:13:00 -0700

    Numbers have life

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,12 +11,12 @@ void print_number(int n)
 		_putchar('-');
 		if (n < -9)
 			print_number(n / -10);
-			_putchar('0' - n % 10);
+		_putchar('0' - n % 10);
 	}
 	else
 	{
 		if (n > 9)
 			print_number(n / 10);
-			_putchar(n % 10 + '0');
+		_putchar(n % 10 + '0');
 	}
 }
