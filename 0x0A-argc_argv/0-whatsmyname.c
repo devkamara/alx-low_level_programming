ebe80e42912a6751f0b0bde05aac89205dae8742
Devkamara <chegerah@gmail.com>
2022-09-30 11:23:38 -0700

    Prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * Return: 0 if success
  */
 
-int _argc(int argc __attribute__((unused)), char* argv[])
+int main(int argc __attribute__((unused)), char* argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
