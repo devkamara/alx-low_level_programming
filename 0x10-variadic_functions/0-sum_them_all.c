3ef99c5b9e9634d9887d88e9c971387eaf5198cb
Devkamara <chegerah@gmail.com>
2022-10-14 14:28:48 -0700

    Returns the sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,7 +16,7 @@ int sum_them_all(const unsigned int n, ...)
 	sum = 0;
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg(ap, unsigned int);
+	sum += va_arg(ap, unsigned int);
 	va_end(ap);
 	return (sum);
 }
