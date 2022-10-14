039a2c422d4a4ba4ff5609ff4d7a64ea95051c45
Devkamara <chegerah@gmail.com>
2022-10-14 13:00:52 -0700

    Spend Too Much Time

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,9 +12,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array != NULL && size > 0 && action != NULL)
-	{
-		for (i = 0; i < size; i++)
+	if (!array || !action)
+		return;
+	for (i = 0; i < size; i++)
 		action(array[i]);
-	}
+
 }
