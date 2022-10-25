3ab85c28a37f6de488bdaea1e194e97ad9018a2c
Devkamara <chegerah@gmail.com>
2022-10-25 14:18:41 -0700

    5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+*func - prints a string
+*/
+void func(void)__attribute__((constructor));
+void func(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
