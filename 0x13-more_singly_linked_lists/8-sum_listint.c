7989268c446f9346f40ee22de04b3925e05b760e
Devkamara <chegerah@gmail.com>
2022-10-26 14:06:30 -0700

    8. Sum list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *sum_listint - returns the sum of all data(n) of listint_t
+ *@head: start point node
+ *Return: 0 if the list is empty
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
