ee5517473af52e2ece6f8def285c4ac19e292f15
Francis Kamara <111047328+devkamara@users.noreply.github.com>
2022-11-01 23:23:02 +0300

    binary no
    converts a binary number to an unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts a binary to unsigned int
+ * @b: pointing to a string of 0 and 1 chars
+ * Return: converted number, 0 if more
+ * than 1 char b is NULL
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+    unsigned int i = 0, num = 0;
+
+    if (b == NULL)
+    return (0);
+    while (b[i])
+    {
+        if (!(b[i] == '0' || b[i] == '1'))
+        return (0);
+        num <<= 1;
+        if (b[i] == '1')
+        {
+            num += 1;
+            i++;
+        }
+        return(num);
+    }
+    
+}
